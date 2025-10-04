83542a857c5faed4cda3c8b6f5621e9d7d02a80d
Karis Caruso <cchristophercmc@gmail.com>
2025-10-04 14:09:07 -0400

    Update countCode.java

--- java/String-2/countCode.java
+++ java/String-2/countCode.java
@@ -2,7 +2,9 @@ public int countCode(String str) {
   int code = 0;
   
   for(int i = 0; i < str.length() - 3; i++) {
-    code += (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e')? 1 : 0;
+    if(str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e') {
+      code++;
+    }
   }
   return code;
 }
