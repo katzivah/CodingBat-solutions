000bcf47d9dfbaf4f58f1bcb6054985d62c9ee7f
Karis Caruso <cchristophercmc@gmail.com>
2025-10-04 14:06:14 -0400

    Update doubleChar.java

--- java/String-2/doubleChar.java
+++ java/String-2/doubleChar.java
@@ -1,6 +1,10 @@
 public String doubleChar(String str) {
   String doubled = "";
   for(int i = 0; i < str.length(); i++) {
-    doubled += str.substring(i, i + 1) + str.substring(i, i + 1);
-  } return doubled;
+    doubled += String.valueOf(str.charAt(i)) + String.valueOf(str.charAt(i));
+    /** 
+      OR -> str.substring(i, i + 1) + str.substring(i, i + 1);
+    */
+  }
+  return doubled;
 }
